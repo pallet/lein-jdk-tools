7287f239c25a1d9f9fb6675781b9bc31815df0e2
Hugo Duncan <hugo@hugoduncan.org>
2012-09-27 17:06:10 -0400

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-jdk-tools "0.1.0"
+(defproject lein-jdk-tools "0.1.1-SNAPSHOT"
   :description "Add tools.jar and sa-jdi.jar to the classpath."
   :url "http://github.com/pallet/lein-jdk-tools"
   :license {:name "Eclipse Public License"
